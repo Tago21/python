335072dc16fd87dc61913edc654009fab17cc95f
Tago21 <mw9977682@gmail.com>
2023-03-23 11:00:16 +0100

    .

--- test.py
+++ test.py
@@ -1,40 +1,40 @@
 a=0
 b=0
 import random 
-print("Witaj zagrajmy w papier, kamien, nozyce ")  
-x=input("Podaj cios ") 
-lista=["kamien","nozyce", "papier"] 
-y=random.choice(lista)  
-print (y) 
-while a==3 or b==3: 
-    print("Koniec") 
-if x==  "kamien" and y=="nozyce": 
-    print("Wygrales") 
-    a=a+1 
-elif x=="nozyce" and y=="papier": 
-    print("Wygrales")
-    a=a+1 
-elif x=="papier" and y=="kamien": 
-    print("Wygrales") 
-    a=a+1 
-elif x=="kamien" and y=="papier": 
-    print("Przegrales") 
-    b=b+1
-elif x=="nozyce" and y=="kamien": 
-    print("Przegrales") 
-    b=b+1
-elif x=="papier" and y=="nozyce": 
-    print("Przegrales") 
-    b=b+1 
-elif x=="papier" and y=="papier":
-    print("Remis ") 
-elif x=="kamien" and y=="kamien": 
-    print("Remis") 
-elif x=="nozyce" and y=="nozyce": 
-    print("Remis") 
-else: 
-    print("error") 
+while a<33 or b<33: 
+    print("Wynik to: ") 
+    print(a) 
+    print(b) 
+    print("Witaj zagrajmy w papier, kamien, nozyce ")  
+    x=input("Podaj cios ") 
+    lista=["kamien","nozyce", "papier"] 
+    y=random.choice(lista)  
+    print (y) 
 
-print ("wynik koncowy to:")
-print (a) 
-print(b) 
+    if x==  "kamien" and y=="nozyce": 
+        print("Wygrales") 
+        a=a+1 
+    elif x=="nozyce" and y=="papier": 
+        print("Wygrales")
+        a=a+1 
+    elif x=="papier" and y=="kamien": 
+        print("Wygrales") 
+        a=a+1 
+    elif x=="kamien" and y=="papier": 
+        print("Przegrales") 
+        b=b+1
+    elif x=="nozyce" and y=="kamien": 
+        print("Przegrales") 
+        b=b+1
+    elif x=="papier" and y=="nozyce": 
+        print("Przegrales") 
+        b=b+1 
+    elif x=="papier" and y=="papier":
+        print("Remis ") 
+    elif x=="kamien" and y=="kamien": 
+        print("Remis") 
+    elif x=="nozyce" and y=="nozyce": 
+        print("Remis") 
+    else: 
+        print("error") 
+
