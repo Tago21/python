f7c076beeeb27c82c47a2def470c2eed8495768a
Tago21 <mw9977682@gmail.com>
2023-03-28 11:19:50 +0200

    .

--- projekt.py
+++ projekt.py
@@ -1,6 +1,21 @@
 #gra zgadnij liczbe 
 import random 
 
-y=random.choice(range(1,100)) 
+y=random.randint(1,100) 
 print("Witaj w grze zgadnij liczbe ") 
-print("Podaj liczbe ") 
+x=int(input ("Podaj liczbe ")) 
+
+while x!=y: 
+
+    if x>y: 
+        print("Za duzo, podaj mniej  ") 
+        x=int(input("Podaj jeszcze raz "))
+    elif x<y: 
+        print("Za malo, podaj wiecej  ") 
+        x=int(input("Podaj jeszcze raz ")) 
+    elif x==y: 
+        print("BRAWO, ZGADLES ") 
+    else: 
+        print("error")      
+print("Brawo WYGRALES ") 
+print("Szukana liczba to: ",y)  
