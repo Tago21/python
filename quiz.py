57de3897e7acdb80b22d2a21ebd9938ac511c6a9
Tago21 <mw9977682@gmail.com>
2023-04-20 11:20:03 +0200

    .

--- quiz.py
+++ quiz.py
@@ -1,7 +1,21 @@
 print("Ile wart jest 1 korona czeska na grosze ") 
 b=0
-a=input('A)15 ') 
-if a=='A' or 'a': 
+a=input("A)0.16 B)0.18 C)0.20 ")  
+
+if a=='A' or a=='a': 
+    print("Bledna  odpowiedz ")  
+
+elif a=='B' or a=='b': 
+    print("Bledna odpowiedz ") 
+elif a=='C' or a=='c': 
+    print("Prawidlowa odpowiedz! Zgarniasz 1 punkt ") 
+    b=b+1 
+else: 
+    print("error")
+print("W ktorym roku byla konstytucja 3 maja ") 
+c=input("A)1791 B)1688 C)1821  ") 
+if a=='A' or a=='a': 
     print("Prawidlowa odpowiedz ") 
     b=b+1 
-elif a=='B' 
+elif a=='B' or a=='b': 
+    print("Bledna odpowiedz ") 
