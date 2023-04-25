46f6f2a1a10a2674aec1dc7fcc5bcead40e5736f
Tago21 <mw9977682@gmail.com>
2023-04-25 11:20:05 +0200

    .

--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
-print("Ile wart jest 1 korona czeska na grosze ") 
+print("1.Ile wart jest 1 korona czeska na zlotowki ") 
 b=0
 a=input("A)0.16 B)0.18 C)0.20 ")  
 
@@ -12,10 +12,47 @@ elif a=='C' or a=='c':
     b=b+1 
 else: 
     print("error")
-print("W ktorym roku byla konstytucja 3 maja ") 
+print("2.W ktorym roku byla konstytucja 3 maja ") 
 c=input("A)1791 B)1688 C)1821  ") 
-if a=='A' or a=='a': 
-    print("Prawidlowa odpowiedz ") 
+if c=='A' or c=='a': 
+    print("Prawidlowa odpowiedz! Zgarniasz 1 punkt  ") 
     b=b+1 
-elif a=='B' or a=='b': 
+elif c=='B' or c=='b': 
     print("Bledna odpowiedz ") 
+elif c=='C' or c=='c': 
+    print("Bledna odpowiedz") 
+else: 
+    print("error") 
+
+print("3.Jaki kraj wygral mistrzostwa swiata w pilce noznej w 2006 roku") 
+d=input("A)Hiszpania B)Niemcy C)Wlochy ") 
+if d=='A' or d=='a': 
+    print("Bledna odpowiedz ") 
+elif d=='B' or d=='b': 
+    print("Bledna odpowiedz ") 
+elif d=='C' or d=='c': 
+    print("Prawidlowa odpowiedz! Zgarniasz 1 punkt") 
+    b=b+1 
+else: 
+    print("error")
+
+print("4.Ile dni ma zwykly rok") 
+e=input("A)366 B)365 C)355 ") 
+if e=='A' or 'a': 
+    print("Bledna odpowiedz ") 
+elif e=='B' or 'b': 
+    print("Prawidlowa odpowiedz! Zgarniasz 1 punkt ") 
+    b=b+1 
+elif e=='C' or e=='c': 
+    print("Bledna odpowiedz ") 
+else: 
+    print("error") 
+
+print("5.Co znaczy po angielsku 'deer'?  ") 
+f=input("A)owca B)jelen C)losos ") 
+
+if f=='A' or f=='a': 
+    print("Bledna odpowiedz ") 
+elif f=='B' or f=='B': 
+    print("Prawidlowa odpowiedz! Zgarniasz 1 punkt ") 
+    b=b+1
